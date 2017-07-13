53a046344de34b13c015b71f68092d91e582c758
kalolo <kalolo@gmail.com>
2017-07-12 17:39:32 -0700

    Adding files

---
+++ index.js
@@ -0,0 +1,63 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var usersArray = [];
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/html/chat.html');
+});
+
+app.get('/chat/:roomId', function(req, res) {
+    console.log(req.params.roomId);
+    var roomId = req.params.roomId;
+    res.send('Room: ' + roomId)
+});
+
+app.get('/userchat/:username', function(req, res){
+    res.sendFile(__dirname + '/html/userchat.html');
+});
+
+app.get('/send', function(req, res){
+    res.sendFile(__dirname + '/html/test.html');
+});
+
+function addUsers(user) { 
+    usersArray.push(user);
+    return usersArray;
+}
+
+function removeUser(user) {
+    usersArray.splize(user, 1);
+    return usersArray;
+
+}
+
+io.on('connection', function(socket) {
+    
+    socket.on('connect-username', function(username){
+        console.log(username, usersArray);
+        socket.username = username;
+        console.log('[+] connect-username', socket.username);
+        io.emit('user-list', addUsers(socket.username));
+    })
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected', socket.username);
+        removeUser(socket.username);
+    });
+
+    socket.on('send-message', function(data){
+        console.log('Message from: '+socket.username+' :', data);
+        io.emit(data.to + '-message', {
+            username: socket.username,
+            message: data.msg
+        });
+    });
+
+
+});
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
